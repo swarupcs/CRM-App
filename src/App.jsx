d1797392b9e8815219cf7a0d6b7173c5e9267781
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-05 22:47:24 +0530

    fix formatting in App component and ensure newline at end of index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
-import Login from "./pages/auth/Login";
+import './App.css';
+import Login from './pages/auth/Login';
 
 function App() {
   return <Login />;
 }
 
 export default App;
-
-
