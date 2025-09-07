c4af8ed7578732f29e567f966a9ea8229e876f67
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 08:54:09 +0530

    Added react hot toast to our app

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
 
 import axiosInstance from '../../config/axiosInstance';
 
@@ -10,8 +11,13 @@ const initialState = {
 
 export const login = createAsyncThunk('/auth/login', async (data) => {
   try {
-    const response = await axiosInstance.post('auth/signin', data);
-    return response;
+    const response = axiosInstance.post('auth/signin', data);
+    toast.promise(response, {
+      loading: 'Submitting form',
+      success: 'Successfully signed in',
+      error: 'Something went wrong, try again',
+    });
+    return await response;
   } catch (error) {
     console.log('printing error', error);
   }
@@ -19,8 +25,13 @@ export const login = createAsyncThunk('/auth/login', async (data) => {
 
 export const signup = createAsyncThunk('/auth/signup', async (data) => {
   try {
-    const response = await axiosInstance.post('auth/signup', data);
-    return response;
+    const response = axiosInstance.post('auth/signup', data);
+    toast.promise(response, {
+      loading: 'Submitting form',
+      success: 'Successfully signed up',
+      error: 'Something went wrong, try again',
+    });
+    return await response;
   } catch (error) {
     console.log('printing error', error);
   }
@@ -32,22 +43,22 @@ const authSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(login.fulfilled, (state, action) => {
-       console.log(action);
-       if (!action.payload) return;
-       state.isLoggedIn = action.payload?.data?.token != undefined;
-       state.data = action.payload?.data?.userData;
-       state.token = action.payload?.data?.token;
-       state.role = action.payload?.data?.userData?.userType;
-       localStorage.setItem('role', action.payload?.data?.userData?.userType);
-       localStorage.setItem(
-         'isLoggedIn',
-         action.payload?.data?.token != undefined
-       );
-       localStorage.setItem(
-         'data',
-         JSON.stringify(action.payload?.data?.userData)
-       ); // "{id: 1, name: "abc"}"
-       localStorage.setItem('token', action.payload?.data?.token);
+      console.log(action);
+      if (!action.payload) return;
+      state.isLoggedIn = action.payload?.data?.token != undefined;
+      state.data = action.payload?.data?.userData;
+      state.token = action.payload?.data?.token;
+      state.role = action.payload?.data?.userData?.userType;
+      localStorage.setItem('role', action.payload?.data?.userData?.userType);
+      localStorage.setItem(
+        'isLoggedIn',
+        action.payload?.data?.token != undefined
+      );
+      localStorage.setItem(
+        'data',
+        JSON.stringify(action.payload?.data?.userData)
+      ); // "{id: 1, name: "abc"}"
+      localStorage.setItem('token', action.payload?.data?.token);
     });
   },
 });
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </BrowserRouter>
 );
