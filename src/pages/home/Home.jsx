e137ac85699ef2762a0dd4a7f13aa86905d52560
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 09:04:10 +0530

    Added home layout with sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,11 @@
+import HomeLayout from '../../layouts/Homelayout';
+
 function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <HomeLayout></HomeLayout>
+    </>
+  );
 }
 
 export default Home;
