a90dcc396516c60e4feb1f8d4a3a807cb1a0c9bb
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 10:04:49 +0530

    Added redirection from the cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,21 @@
+import { AiOutlineDownload } from 'react-icons/ai';
+import { usePDF } from 'react-to-pdf';
+
 import useTickets from '../hooks/useTickets';
 import HomeLayout from '../layouts/Homelayout';
-import { AiOutlineDownload } from "react-icons/ai";
-import { usePDF } from "react-to-pdf";
 function Dashboard() {
   const [ticketState] = useTickets();
-
-   const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
 
   return (
     <HomeLayout>
       <div className='min-h-[90vh] flex flex-col items-center justify-center gap-2'>
         <div className='bg-yellow-500 w-full text-black text-center text-3xl py-4 font-bold hover:bg-yellow-400 transition-all ease-in-out duration-300'>
-           Tickets Records <AiOutlineDownload className="cursor-pointer inline " onClick={() => toPDF()} />
+          Tickets Records{' '}
+          <AiOutlineDownload
+            className='cursor-pointer inline '
+            onClick={() => toPDF()}
+          />
         </div>
 
         {/* Table */}
