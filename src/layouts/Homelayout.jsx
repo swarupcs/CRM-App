8bf85747c6bfb6025eb9c69c2bb8e252e129aa7c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 10:57:20 +0530

    create ticket ui impl done

--- src/layouts/Homelayout.jsx
+++ src/layouts/Homelayout.jsx
@@ -41,7 +41,11 @@ function HomeLayout({ children }) {
             <li>
               <Link to='/dashboard'>Dashboard</Link>
             </li>
-
+            {authState.isLoggedIn && (
+              <li>
+                <Link to='/ticket/create'>Create Ticket</Link>
+              </li>
+            )}
             {authState.role === 'admin' && (
               <li>
                 <Link to='/users'>All Users</Link>
