df541b337f6704f56ad0c00b725fa064ca65dc9c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 10:47:35 +0530

    Added auth routes component

--- src/routing/MainRoutes.jsx
+++ src/routing/MainRoutes.jsx
@@ -4,13 +4,16 @@ import Login from '../pages/auth/Login';
 import Signup from '../pages/auth/Signup';
 import Dashboard from '../pages/Dashboard';
 import Home from '../pages/home/Home';
-
+import AuthRoutes from './AuthRoutes';
 function MainRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
+      <Route element={<AuthRoutes allowListedRoles={['engineer']} />}>
+        <Route path='/resolve' element={<div>Testing</div>} />
+      </Route>
       <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   );
